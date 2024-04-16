82a8c3203c66293c316d5aea98f9962141229d13
robotcoders <167171955+robotcoders@users.noreply.github.com>
2024-04-16 15:42:48 +0800

    Update Arduino_IIC_0_96_2OLED_0x3D_0x3C.ino

--- Arduino_IIC_0_96_2OLED_0x3D_0x3C/Arduino_IIC_0_96_2OLED_0x3D_0x3C.ino
+++ Arduino_IIC_0_96_2OLED_0x3D_0x3C/Arduino_IIC_0_96_2OLED_0x3D_0x3C.ino
@@ -4,35 +4,35 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
-U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2_1(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); // 第一个屏幕对象
-U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2_2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); // 第二个屏幕对象
+U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2_1(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); // First screen object
+U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2_2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); // Second screen object
 
 void setup() {
   Wire.begin();
   
-  // 初始化第一个 OLED 屏幕，地址为 0x3D
+  // Initialize the first OLED screen, address 0x3D
   u8g2_1.setI2CAddress(0x3D << 1);
   u8g2_1.begin();
   
-  // 初始化第二个 OLED 屏幕，地址为 0x3C
+  // Initialize the second OLED screen, address 0x3C
   u8g2_2.setI2CAddress(0x3C << 1);
   u8g2_2.begin();
 }
 
 void loop() {
-  // 在屏幕1上显示 "OLED1"
+  // Display "OLED1" on screen 1
   u8g2_1.firstPage();
   do {
     u8g2_1.setFont(u8g2_font_ncenB10_tr);
     u8g2_1.drawStr(0, 15, "OLED1");
   } while (u8g2_1.nextPage());
   
-  // 在屏幕2上显示 "OLED2"
+  // Display "OLED2" on screen 2
   u8g2_2.firstPage();
   do {
     u8g2_2.setFont(u8g2_font_ncenB10_tr);
     u8g2_2.drawStr(0, 15, "OLED2");
   } while (u8g2_2.nextPage());
   
-  delay(2000); // 延时2秒
+  delay(2000); // Delay for 2 seconds
 }
